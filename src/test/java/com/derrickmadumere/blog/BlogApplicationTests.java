a8feac20093c0ce20a963af3674e4c135f05e1b2
Kachielite <40024356+Kachielite@users.noreply.github.com>
2024-02-06 20:22:36 +0300

    initiated git

---
+++ src/test/java/com/derrickmadumere/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derrickmadumere.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
